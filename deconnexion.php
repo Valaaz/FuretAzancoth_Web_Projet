8ec96b532bfa557bfa45ddfb29bd9cb484160f91
Raigun57 <62474714+Raigun57@users.noreply.github.com>
2020-10-27 11:46:07 +0100

    Update deconnexion.php

--- deconnexion.php
+++ deconnexion.php
@@ -1,8 +1,27 @@
 <?php
     session_start();
+	
     if (isset($_SESSION['isLogged']))
     {
         session_unset();
         session_destroy();
     }
-    header('Location:accueil.php');
+   ?> header('Location:accueil.php');
+	
+	<html>
+
+<head>
+    <title>Déconnexion</title>
+</head>
+
+<body>
+</body>
+<script language=" javascript " type="text/javascript">
+        function MessageAlerte(message)
+        {
+            if ( confirm( "Message à afficher" ) ) {} 
+			else {}
+        }
+    </script>
+
+</html>
