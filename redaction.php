69529c1cfcad5c65d01d7fda7049d731c32b936e
Valentin AZANCOTH <62429527+Valaaz@users.noreply.github.com>
2020-10-29 18:04:26 +0100

    Update redaction.php

--- redaction.php
+++ redaction.php
@@ -1,45 +1,68 @@
 <?php
 include 'connexion.php';
 
-$pdostat = $objPdo->prepare("INSERT INTO `news`(`titrenews`, `textenews`) VALUES(:titrenews, :textenews);");
-$pdostat->bindvalue(':titrenews', $titrenews, PDO::PARAM_STR);
-$pdostat->bindvalue(':textenews', $textenews, PDO::PARAM_STR);
-if (!$pdostat->execute()) { // Si le résultat de la requête est faux ou null, c'est qu'il y a eu un problème
-                $err_inscription[] = 'Erreur MySQL.';
+$creertheme = $_POST['creer_theme'];
+$titre = $_POST['titre'];
+$contenu = $_POST['contenu'];
+
+$err_news = array();
+if (isset($_POST['valider'])) {
+    if (!empty($_POST['contenu']) && empty($_POST['material'])) {
+        if (!empty($_POST['titre']) && !empty($_POST['contenu']) && !empty($_POST['material'])) {
+            $pdostat = $objPdo->prepare("INSERT INTO `news`(`idtheme`, `titrenews`, `textenews`, `idredacteur`) VALUES (:creertheme, :titre, :contenu, :redacteur);");
+            $pdostat->bindvalue(':creertheme', $creertheme, PDO::PARAM_INT);
+            $pdostat->bindvalue(':titre', $titre, PDO::PARAM_STR);
+            $pdostat->bindvalue(':contenu', $contenu, PDO::PARAM_STR);
+            $pdostat->bindvalue(':redacteur', 5, PDO::PARAM_INT);
+            $err_news[] = 'Coucou';
+            header('location:redaction.php');
+        } else {
+            $err_news[] = 'Remplir les champs vides';
+        }
+    } else if (!empty($_POST['titre']) && !empty($_POST['contenu']) && !empty($_POST['material'])) {
+        $pdostat = $objPdo->prepare("INSERT INTO `news`(`idtheme`, `titrenews`, `textenews`, `idredacteur`) VALUES (:creertheme, :titre, :contenu, :redacteur);");
+        $pdostat->bindvalue(':creertheme', $creertheme, PDO::PARAM_INT);
+        $pdostat->bindvalue(':titre', $titre, PDO::PARAM_STR);
+        $pdostat->bindvalue(':contenu', $contenu, PDO::PARAM_STR);
+        $pdostat->bindvalue(':redacteur', 5, PDO::PARAM_INT);
+        $err_news[] = 'Coucou 2';
+        header('location:redaction.php');
+    }
 }
 ?>
 <html>
 
 <head>
     <title>Rédaction</title>
-    Bonjour rédacteur !
 </head>
 
 <body>
-    <form method="post" action="creernews.php">
+    <form method="post" action="redaction.php">
+        <?php if (!empty($err_news)) { ?>
+            <div class="error"><?php echo implode('<br/>', $err_news); ?></div>
+        <?php     } ?>
         <h1>Créer une news</h1>
         <label>Titre</label> </br>
         <input type="text" value="" name="titre"> </br>
 
         <label>Thème</label> </br>
+        <input type="text" value="" name="creer_theme"> </br> </br>
 
+        <label>Choisir un thème</label> </br>
         <select name='material'>
             <option value=''></option>
 
             <?php
-           
-
             $query = $objPdo->prepare("SELECT * FROM theme");
             $query->execute();
             foreach ($query as $row) {
                 echo '<option value="' . $row['description'] . '">' . $row['description'] . '</option>';
             }
             ?>
-        </select> </br>
+        </select> </br> </br>
 
         <label>Contenu</label> </br>
         <textarea name="contenu" id="contenu" rows="40" cols="100"> </textarea> </br>
-		
 
         <input type="submit" value="Validez" name="valider">
     </form>
