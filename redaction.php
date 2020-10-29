7154ffb6c00caee25ae061329d89db4d6a542705
Valentin AZANCOTH <62429527+Valaaz@users.noreply.github.com>
2020-10-29 14:58:09 +0100

    Combobox de thème initialisée

--- redaction.php
+++ redaction.php
@@ -2,14 +2,11 @@
 
 <head>
     <title>Rédaction</title>
-	Bonjour rédacteur !
+    Bonjour rédacteur !
 </head>
 
 <body>
     <form method="post" action="creernews.php">
-        <?php if (!empty($err_inscription)) { ?>
-            <div class="error"><?php echo implode('<br/>', $err_inscription); ?></div>
-        <?php     } ?>
         <h1>Créer une news</h1>
         <label>Titre</label> </br>
         <input type="text" value="" name="titre"> </br>
@@ -17,9 +14,23 @@
         <label>Thème</label> </br>
         <input type="text" value="" name="theme"> </br>
 
+        <select name='material'>
+            <option value=''></option>
+
+            <?php
+            include 'connexion.php';
+
+            $query = $objPdo->prepare("SELECT * FROM theme");
+            $query->execute();
+            foreach ($query as $row) {
+                echo '<option value="' . $row['description'] . '">' . $row['description'] . '</option>';
+            }
+            ?>
+        </select>
+
         <label>Contenu</label> </br>
         <input type="text" value="" name="contenu"> </br>
-		
+
         <input type="submit" value="Validez" name="valider">
     </form>
 </body>
