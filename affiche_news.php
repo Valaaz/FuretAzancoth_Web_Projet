6864ac2c20b63962ae523ba6b1bd306cac815e19
Valentin AZANCOTH <62429527+Valaaz@users.noreply.github.com>
2020-11-02 15:17:12 +0100

    Le nom du rédacteur s'affiche

--- affiche_news.php
+++ affiche_news.php
@@ -13,16 +13,10 @@
             $titre = $row['titrenews'];
             $contenu = $row['textenews'];
             $date = $row['datenews'];
+            $idredacteur = $row['idredacteur'];
         }
 
         // Partie theme
-        /*
-        $querytheme = $objPdo->prepare("SELECT idtheme FROM news WHERE idnews = :idnews");
-        $querytheme->bindValue(':idnews', $idnews, PDO::PARAM_INT);
-        $idtheme = $querytheme->execute();
-        echo '<br> 2 : Theme en cours : ' . $idtheme;
-        */
-
         $resulttheme = $objPdo->prepare("SELECT description FROM theme WHERE idtheme = :idtheme");
         $resulttheme->bindvalue(':idtheme', $theme, PDO::PARAM_INT);
         $resulttheme->execute();
@@ -30,17 +24,13 @@
             $theme = $row2['description'];
 
         // Partie redacteur
-        /*
-        $queryredac = $objPdo->query("SELECT idredacteur FROM news WHERE idnews = 1");
-        $idredacteur = $queryredac->execute();
-
         $resultredac = $objPdo->prepare("SELECT pseudo FROM redacteur WHERE idredacteur = :idredacteur");
         $resultredac->bindvalue(':idredacteur', $idredacteur, PDO::PARAM_INT);
         $resultredac->execute();
         while ($row3 = $resultredac->fetch())
-        $redacteur = $row3['pseudo'];
-        */
-        echo 'Rédigé le ' . $date . ' par ';
+            $redacteur = $row3['pseudo'];
+
+        echo 'Rédigé le ' . $date . ' par ' . $redacteur;
 
         echo '<h1>Thème :</h1>';
         echo  $theme . '<br>';
