1c2173621f35361bf174539c09e964a9dcc2256c
Valentin AZANCOTH <62429527+Valaaz@users.noreply.github.com>
2020-11-05 21:54:28 +0100

    Regex amélioré

--- creercompte.php
+++ creercompte.php
@@ -6,10 +6,10 @@ $query = $objPdo->prepare("SELECT * FROM redacteur");
 $query->execute();
 
 $ok = true;
-
+echo 'pas joli';
 $err_inscription = array();
 if (isset($_POST['valider'])) {
-    // Vérifie si les champs sont remplis on non 
+    echo 'joli';
     if (!empty($_POST['pseudo']) && !empty($_POST['mail']) && !empty($_POST['mdp']) && !empty($_POST['mdp2']) && !empty($_POST['nom']) && !empty($_POST['prenom'])) {
         // Vérifie si le pseudo et/ou le mail rentré(-s) existe(-nt) déjà
         foreach ($query as $row) {
@@ -22,41 +22,37 @@ if (isset($_POST['valider'])) {
                 $ok = false;
             }
         }
-        //$ok = false;
         if ($ok == true) {
-            if ($_POST['mdp'] == $_POST['mdp2']) {
 
-                $nom = htmlentities($_POST['nom']);
-                $prenom = htmlentities($_POST['prenom']);
-                $pseudo = htmlentities($_POST['pseudo']);
-                $mail = htmlentities($_POST['mail']);
-                $mdp = crypt(htmlentities($_POST['mdp']), '$2a$07$usesomesillystringforsalt');
+            $nom = htmlentities($_POST['nom']);
+            $prenom = htmlentities($_POST['prenom']);
+            $pseudo = htmlentities($_POST['pseudo']);
+            $mail = htmlentities($_POST['mail']);
+            $mdp = crypt(htmlentities($_POST['mdp']), '$2a$07$usesomesillystringforsalt');
 
-                // INSERT en bdd
-                $pdostat = $objPdo->prepare("INSERT INTO `redacteur`(`nom`, `prenom`, `pseudo`, `adressemail`, `motdepasse`) VALUES(:nom, :prenom, :pseudo, :mail, :mdp);");
-                $pdostat->bindvalue(':pseudo', $pseudo, PDO::PARAM_STR);
-                $pdostat->bindvalue(':mdp', $mdp, PDO::PARAM_STR);
-                $pdostat->bindvalue(':nom', $nom, PDO::PARAM_STR);
-                $pdostat->bindvalue(':prenom', $prenom, PDO::PARAM_STR);
-                $pdostat->bindvalue(':mail', $mail, PDO::PARAM_STR);
+            // INSERT en bdd
+            $pdostat = $objPdo->prepare("INSERT INTO `redacteur`(`nom`, `prenom`, `pseudo`, `adressemail`, `motdepasse`) VALUES(:nom, :prenom, :pseudo, :mail, :mdp);");
+            $pdostat->bindvalue(':pseudo', $pseudo, PDO::PARAM_STR);
+            $pdostat->bindvalue(':mdp', $mdp, PDO::PARAM_STR);
+            $pdostat->bindvalue(':nom', $nom, PDO::PARAM_STR);
+            $pdostat->bindvalue(':prenom', $prenom, PDO::PARAM_STR);
+            $pdostat->bindvalue(':mail', $mail, PDO::PARAM_STR);
 
-                if (!$pdostat->execute()) { // Si le résultat de la requête est faux ou null, c'est qu'il y a eu un problème
-                    $err_inscription[] = 'Erreur MySQL : Ce pseudo ou cette adresse mail existe déjà.';
-                } else {
-                    if (session_id() == "") // Si l'id de session est vide, c'est que la session n'est pas démarée
-                        session_start();
-                    // Mise en SESSION
-                    $_SESSION['isLogged'] = true;
-                    $_SESSION['pseudo'] = $pseudo;
-                    $_SESSION['mail'] = $mail;
+            if (!$pdostat->execute()) { // Si le résultat de la requête est faux ou null, c'est qu'il y a eu un problème
+                $err_inscription[] = 'Erreur MySQL : Ce pseudo ou cette adresse mail existe déjà.';
+            } else {
+                if (session_id() == "") // Si l'id de session est vide, c'est que la session n'est pas démarée
+                    session_start();
+                // Mise en SESSION
+                $_SESSION['isLogged'] = true;
+                $_SESSION['pseudo'] = $pseudo;
+                $_SESSION['mail'] = $mail;
 
-                    // ATTENTION ! ON NE MET JAMAIS LE MOT DE PASSE EN SESSION !!
-                    // on redirige vers l'espace membre
-                    header('location:accueil.php');
-                    exit();
-                }
-            } else
-                $err_inscription[] = 'Les deux mots de passe sont différents.';
+                // ATTENTION ! ON NE MET JAMAIS LE MOT DE PASSE EN SESSION !!
+                // on redirige vers l'espace membre
+                header('location:accueil.php');
+                exit();
+            }
         }
     } else
         $err_inscription[] = 'Remplissez tous les champs obligatoires.';
@@ -73,7 +69,7 @@ if (isset($_POST['valider'])) {
 <body>
     <header>
     </header>
-    <form method="post" action="creercompte.php" onsubmit="return VerifMail()" name="formulaire">
+    <form method="post" action="creercompte.php" onsubmit="return Valider()" name="formulaire">
         <?php if (!empty($err_inscription)) { ?>
             <div class="error"><?php echo implode('<br/>', $err_inscription); ?></div>
         <?php     } ?>
@@ -107,10 +103,11 @@ if (isset($_POST['valider'])) {
             window.location.href = "accueil.php"
     }
 
-    function VerifMail() {
+    function Valider() {
 
         var nom = document.forms['formulaire'].nom
         var prenom = document.forms['formulaire'].prenom;
+        var pseudo = document.forms['formulaire'].pseudo;
         var mail = document.forms['formulaire'].mail;
         var mdp = document.forms['formulaire'].mdp;
         var mdp2 = document.forms['formulaire'].mdp2;
@@ -133,6 +130,13 @@ if (isset($_POST['valider'])) {
         } else
             ReinitialiserCouleur(prenom);
 
+        if (!pseudo.value.replace(/\s+/, '').length) {
+            alert("Pseudo vide");
+            ok = false;
+            ChangerCouleur(pseudo);
+        } else
+            ReinitialiserCouleur(pseudo);
+
         if (!mail.value.replace(/\s+/, '').length) {
             alert("Mail vide");
             ok = false;
@@ -144,15 +148,13 @@ if (isset($_POST['valider'])) {
             alert("Mot de passe vide");
             ok = false;
             ChangerCouleur(mdp);
-        } else
-            ReinitialiserCouleur(mdp);
+        }
 
         if (!mdp2.value.replace(/\s+/, '').length) {
             alert("Confirmation mot de passe vide");
             ok = false;
             ChangerCouleur(mdp2);
-        } else
-            ReinitialiserCouleur(mdp2);
+        }
 
         if (!mail.value.match(regex)) {
             alert("Mail pas bon");
@@ -165,7 +167,7 @@ if (isset($_POST['valider'])) {
         if (mdp.value != mdp2.value) {
             alert("Les deux mots de passe sont différents");
             ok = false;
-            ChangerCouleur(mdp1);
+            ChangerCouleur(mdp);
             ChangerCouleur(mdp2);
         } else {
             ReinitialiserCouleur(mdp);
