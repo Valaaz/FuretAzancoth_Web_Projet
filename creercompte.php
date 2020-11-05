537d2bed03843d1f905345a60e25980f1f688dbe
Valentin AZANCOTH <62429527+Valaaz@users.noreply.github.com>
2020-11-05 21:56:15 +0100

    Code inutile enlever

--- creercompte.php
+++ creercompte.php
@@ -6,56 +6,51 @@ $query = $objPdo->prepare("SELECT * FROM redacteur");
 $query->execute();
 
 $ok = true;
-echo 'pas joli';
 $err_inscription = array();
 if (isset($_POST['valider'])) {
-    echo 'joli';
-    if (!empty($_POST['pseudo']) && !empty($_POST['mail']) && !empty($_POST['mdp']) && !empty($_POST['mdp2']) && !empty($_POST['nom']) && !empty($_POST['prenom'])) {
-        // Vérifie si le pseudo et/ou le mail rentré(-s) existe(-nt) déjà
-        foreach ($query as $row) {
-            if ($row['pseudo'] == $_POST['pseudo']) {
-                $err_inscription[] = 'Ce pseudo existe déjà';
-                $ok = false;
-            }
-            if ($row['adressemail'] == $_POST['mail']) {
-                $err_inscription[] = 'Cette adresse mail existe déjà';
-                $ok = false;
-            }
+    // Vérifie si le pseudo et/ou le mail rentré(-s) existe(-nt) déjà
+    foreach ($query as $row) {
+        if ($row['pseudo'] == $_POST['pseudo']) {
+            $err_inscription[] = 'Ce pseudo existe déjà';
+            $ok = false;
         }
-        if ($ok == true) {
-
-            $nom = htmlentities($_POST['nom']);
-            $prenom = htmlentities($_POST['prenom']);
-            $pseudo = htmlentities($_POST['pseudo']);
-            $mail = htmlentities($_POST['mail']);
-            $mdp = crypt(htmlentities($_POST['mdp']), '$2a$07$usesomesillystringforsalt');
-
-            // INSERT en bdd
-            $pdostat = $objPdo->prepare("INSERT INTO `redacteur`(`nom`, `prenom`, `pseudo`, `adressemail`, `motdepasse`) VALUES(:nom, :prenom, :pseudo, :mail, :mdp);");
-            $pdostat->bindvalue(':pseudo', $pseudo, PDO::PARAM_STR);
-            $pdostat->bindvalue(':mdp', $mdp, PDO::PARAM_STR);
-            $pdostat->bindvalue(':nom', $nom, PDO::PARAM_STR);
-            $pdostat->bindvalue(':prenom', $prenom, PDO::PARAM_STR);
-            $pdostat->bindvalue(':mail', $mail, PDO::PARAM_STR);
-
-            if (!$pdostat->execute()) { // Si le résultat de la requête est faux ou null, c'est qu'il y a eu un problème
-                $err_inscription[] = 'Erreur MySQL : Ce pseudo ou cette adresse mail existe déjà.';
-            } else {
-                if (session_id() == "") // Si l'id de session est vide, c'est que la session n'est pas démarée
-                    session_start();
-                // Mise en SESSION
-                $_SESSION['isLogged'] = true;
-                $_SESSION['pseudo'] = $pseudo;
-                $_SESSION['mail'] = $mail;
-
-                // ATTENTION ! ON NE MET JAMAIS LE MOT DE PASSE EN SESSION !!
-                // on redirige vers l'espace membre
-                header('location:accueil.php');
-                exit();
-            }
+        if ($row['adressemail'] == $_POST['mail']) {
+            $err_inscription[] = 'Cette adresse mail existe déjà';
+            $ok = false;
         }
-    } else
-        $err_inscription[] = 'Remplissez tous les champs obligatoires.';
+    }
+    if ($ok == true) {
+
+        $nom = htmlentities($_POST['nom']);
+        $prenom = htmlentities($_POST['prenom']);
+        $pseudo = htmlentities($_POST['pseudo']);
+        $mail = htmlentities($_POST['mail']);
+        $mdp = crypt(htmlentities($_POST['mdp']), '$2a$07$usesomesillystringforsalt');
+
+        // INSERT en bdd
+        $pdostat = $objPdo->prepare("INSERT INTO `redacteur`(`nom`, `prenom`, `pseudo`, `adressemail`, `motdepasse`) VALUES(:nom, :prenom, :pseudo, :mail, :mdp);");
+        $pdostat->bindvalue(':pseudo', $pseudo, PDO::PARAM_STR);
+        $pdostat->bindvalue(':mdp', $mdp, PDO::PARAM_STR);
+        $pdostat->bindvalue(':nom', $nom, PDO::PARAM_STR);
+        $pdostat->bindvalue(':prenom', $prenom, PDO::PARAM_STR);
+        $pdostat->bindvalue(':mail', $mail, PDO::PARAM_STR);
+
+        if (!$pdostat->execute()) { // Si le résultat de la requête est faux ou null, c'est qu'il y a eu un problème
+            $err_inscription[] = 'Erreur MySQL : Ce pseudo ou cette adresse mail existe déjà.';
+        } else {
+            if (session_id() == "") // Si l'id de session est vide, c'est que la session n'est pas démarée
+                session_start();
+            // Mise en SESSION
+            $_SESSION['isLogged'] = true;
+            $_SESSION['pseudo'] = $pseudo;
+            $_SESSION['mail'] = $mail;
+
+            // ATTENTION ! ON NE MET JAMAIS LE MOT DE PASSE EN SESSION !!
+            // on redirige vers l'espace membre
+            header('location:accueil.php');
+            exit();
+        }
+    }
 }
 ?>
 
